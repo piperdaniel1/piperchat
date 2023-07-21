ab36e5dfeaa4775eb1053e7ee4000a87bb6f39e5
Daniel Piper <piperdaniel1@gmail.com>
2023-07-20 18:47:44 -0700

    sample

--- frontend/src/Chat.jsx
+++ frontend/src/Chat.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react';
 import './Chat.css';
 import { ReactMarkdown } from 'react-markdown/lib/react-markdown';
 
+const API_URL = 'http://192.168.10.14:3000';
+
 const Message = ({ text, sender }) => {
   return (
     <div className={`message ${sender}-message`}>
@@ -10,15 +12,31 @@ const Message = ({ text, sender }) => {
   );
 };
 
+
 const Chat = () => {
   const messages = [
-    {
-      text: 'Hello, how can I help you today?',
-      sender: 'ai',
-    },
   ];
 
   const [messageList, setMessageList] = useState(messages);
+
+  async function getNextResponse(newList) {
+    // post request
+    const messages = newList.map((message) => message.text);
+    const response = await fetch(`${API_URL}/query`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ messages }),
+    });
+    
+    const newNewList = [...newList];
+    newNewList.push({
+      text: (await response.json()).response,
+      sender: 'ai',
+    });
+    setMessageList(newNewList);
+  }
 
   return (
   <div className='outer-chat-container'>
@@ -37,8 +55,9 @@ const Chat = () => {
         type="text"
         placeholder="Type your message here"
         id="message-input"
-        onKeyDown={(event) => {
-          if (event.key === 'Enter' && !event.shiftKey) {
+        onKeyDown={async (event) => {
+          if (event.key === 'Enter' && !event.shiftKey && (messageList.length === 0 || messageList[messageList.length - 1].sender !== 'user')) {
+            
             // Handle enter key press
             console.log('Enter key pressed');
             event.preventDefault();
@@ -49,7 +68,7 @@ const Chat = () => {
             });
             setMessageList(newMessageList);
             event.target.value = '';
-            // send to backend
+            await getNextResponse(newMessageList);
           } else if (event.key === 'Enter' && event.shiftKey) {
             // Handle shift+enter key press
             console.log('Shift+Enter key pressed');
